60cdef7ac6cb2bdc33b0eccd16386c5168dc64cb
jingyu <wlsrb97@naver.com>
2026-04-22 22:49:20 +0900

    chore: initialize project setup

---
+++ src/main/java/com/example/graphqlgrpcsample/GraphqlGrpcSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphqlgrpcsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlGrpcSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlGrpcSampleApplication.class, args);
+    }
+
+}
