1f0776c0f859b4dd81409c26fc81e26862c936d3
jingyu <wlsrb97@naver.com>
2026-04-27 23:14:21 +0900

    refactor: remove image mutations from graphql api

--- src/main/java/com/example/graphqlsample/image/service/ImageCommandService.java
+++ src/main/java/com/example/graphqlsample/image/service/ImageCommandService.java
@@ -1,7 +1,5 @@
 package com.example.graphqlsample.image.service;
 
-import com.example.graphqlsample.image.graphql.dto.ImageMutationDto.CreateUploadResponse;
-import com.example.graphqlsample.image.proto.CreateImageUploadRequest;
 import com.example.graphqlsample.image.proto.DeleteImageRequest;
 import com.example.graphqlsample.image.proto.ImageServiceGrpc;
 import lombok.RequiredArgsConstructor;
@@ -12,17 +10,6 @@ import org.springframework.stereotype.Service;
 public class ImageCommandService {
 
     private final ImageServiceGrpc.ImageServiceBlockingStub imageServiceBlockingStub;
-
-    public CreateUploadResponse createImageUpload(String originalFileName, String contentType) {
-        var response = imageServiceBlockingStub.createImageUpload(
-                CreateImageUploadRequest.newBuilder()
-                        .setOriginalFileName(originalFileName)
-                        .setContentType(contentType)
-                        .build()
-        );
-
-        return new CreateUploadResponse(response.getImageId(), response.getUploadUrl());
-    }
 
     public void deleteImage(String imageId) {
         imageServiceBlockingStub.deleteImage(
